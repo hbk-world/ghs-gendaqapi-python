d87d966494db271123ecfc247eda55ebd98485be
PTMargarido <46863913+PTMargarido@users.noreply.github.com>
2021-12-08 09:26:06 +0000

    CI integration for unit test run and coverage
    * [CI] - Added CI running unit tests
    
    * [CI] - Added Unit Test coverage
    
    * [CI] - Fixed path when running unittests
    
    * [CI] - Added generation of coverage xml
    
    * [CI] - Removed unecessary package
    
    Co-authored-by: Pedro Margarido <margarido@hbm.com>

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# Example Package
+[![Build Status](https://dev.azure.com/GenesisHighSpeed/GHS%20ESW/_apis/build/status/hbk-world.ghs-gendaqapi-python?branchName=main)](https://dev.azure.com/GenesisHighSpeed/GHS%20ESW/_build/latest?definitionId=117&branchName=main)
 
+# Example Package
 This is a simple example package. You can use
 [Github-flavored Markdown](https://guides.github.com/features/mastering-markdown/)
 to write your content.
--- unittest/test_suite.py
+++ unittest/test_suite.py
@@ -1,33 +1,47 @@
 """Gen Daq Python API unit test suite."""
-import sys
 import unittest
+import sys
 
 from HtmlTestRunner import HTMLTestRunner
+from xmlrunner import XMLTestRunner
 
-import test_acquisition_api
-import test_connection_api
 import test_connection_handler
 import test_json
+import test_connection_api
+import test_acquisition_api
 import test_mainframe_api
 
-sys.tracebacklimit = 0
-# initialize the test suite
-loader = unittest.TestLoader()
-suite = unittest.TestSuite()
+if __name__ == '__main__':
 
-# add tests to the test suite
-suite.addTests(loader.loadTestsFromModule(test_json))
-suite.addTests(loader.loadTestsFromModule(test_connection_handler))
-suite.addTests(loader.loadTestsFromModule(test_connection_api))
-suite.addTests(loader.loadTestsFromModule(test_acquisition_api))
-suite.addTests(loader.loadTestsFromModule(test_mainframe_api))
+    sys.tracebacklimit = 0
+    # initialize the test suite
+    loader = unittest.TestLoader()
+    suite = unittest.TestSuite()
+
+    # add tests to the test suite
+    suite.addTests(loader.loadTestsFromModule(test_json))
+    suite.addTests(loader.loadTestsFromModule(test_connection_handler))
+    suite.addTests(loader.loadTestsFromModule(test_connection_api))
+    suite.addTests(loader.loadTestsFromModule(test_acquisition_api))
+    suite.addTests(loader.loadTestsFromModule(test_mainframe_api))
 
 
-# initialize a runner, pass it your suite and run it
-runner = HTMLTestRunner(
-    combine_reports=True,
-    open_in_browser=True,
-    report_name="Python Driver Unittest Report",
-    report_title="Python Driver Unittest Report",
-)
-runner.run(suite)
+    # initialize a runner, pass it your suite and run it
+    HTMLTestRunner(
+        combine_reports=True,
+        open_in_browser=False,
+        report_name="Python Driver Unittest Report",
+        report_title="Python Driver Unittest Report",
+    ).run(suite)
+
+    suite = unittest.TestSuite()
+
+    # add tests to the test suite
+    suite.addTests(loader.loadTestsFromModule(test_json))
+    suite.addTests(loader.loadTestsFromModule(test_connection_handler))
+    suite.addTests(loader.loadTestsFromModule(test_connection_api))
+    suite.addTests(loader.loadTestsFromModule(test_acquisition_api))
+    suite.addTests(loader.loadTestsFromModule(test_mainframe_api))
+
+    result = not XMLTestRunner(output='reports').run(suite).wasSuccessful()
+    sys.exit(result)
